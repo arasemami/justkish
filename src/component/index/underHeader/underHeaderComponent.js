ec2bfed479c679adacb4ff91d7657b5c9540e1ad
aras armani <aras.armani2014@gmail.com>
2019-05-13 12:12:44 +0300

    under header is fixed

---
+++ src/component/index/underHeader/underHeaderComponent.js
@@ -0,0 +1,23 @@
+import React , { Component } from 'react';
+import './style.css';
+
+
+
+class UnderHeaderComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="container-fluid">
+                <div className="container">
+                    <p>Under header</p>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default UnderHeaderCompone
+<div>nt;
