5ee39bbe1e2679715bed304719a620ad002f66b7
aras armani <aras.armani2014@gmail.com>
2019-07-01 11:29:03 +0300

    fixed all

--- src/container/search/search.js
+++ src/container/search/search.js
@@ -14,23 +14,23 @@ class SearchContainer extends Component {
     }
     render() { 
         return ( 
-            <div className="contaniner-flui bg-f9f9f9">
+            <div className="container-fluid bg-f9f9f9">
           
                 <div className="container">
                     <HeaderSearchResualt />
                     <div className="row-flex">
                         <div className="search-post-box">
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
-                            <CardsComponent title="تورهای زیبا"/> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
+                            <CardsComponent title="تورهای زیبا" /> 
                         </div>
                     </div>
                 </div>
